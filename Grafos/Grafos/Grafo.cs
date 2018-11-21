88091b82c58caa43875d091f15b45b750e49efd6
LuisEstrada33 <43280434+LuisEstrada33@users.noreply.github.com>
2018-11-20 17:56:51 -0800

    Add files via upload

---
+++ Grafos/Grafos/Grafo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafos
+{
+    class Grafo
+    {
+
+        public void Rutas(int ruta) //Método que recibe el parametro ruta para elegir cual desplegar
+        {
+            switch (ruta) //Depende de 'ruta' se despliega la misma
+            {
+                case 1:
+                    Console.WriteLine("A -> B -> C -> D -> E -> F -> G");
+                    break;
+                case 2:
+                    Console.WriteLine("A -> B -> G -> F -> E -> D -> C");
+                    break;
+                case 3:
+                    Console.WriteLine("A -> B -> C -> G -> F -> D -> E");
+                    break;                                                      //Rutas disponibles
+                case 4:
+                    Console.WriteLine("A -> B -> G -> C -> D -> E -> F");
+                    break;
+                case 5:
+                    Console.WriteLine("A -> B -> C -> G -> F -> E -> D");
+                    break;
+                case 6:
+                    Console.WriteLine("A -> B -> G -> C -> D -> F -> E");
+                    break;
+            }
+        }
+    }
+}
---
+++ Grafos/Grafos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grafos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Grafo llamar = new Grafo(); //Hace la instanciación del objeto
+            Console.WriteLine("Las rutas son:\n");
+            for (int contador = 1; contador < 7; contador++)
+            {
+                Console.Write("\t");  //Agrega los tab a la consola
+                llamar.Rutas(contador);//Llama al metodo para desplegar las rutas
+            }
+            Console.ReadKey();
+        }
+    }
+}
